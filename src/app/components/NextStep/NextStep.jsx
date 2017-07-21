b9a9066687190e2bd9f32e460c24c4d5b36d8a46
nmelnychenko <m.melnychenko@lectrum.io>
2017-07-21 16:30:01 +0300

    start work using react-redux boilerplate

---
+++ src/app/components/NextStep/NextStep.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PreviousStep = () => (
+    <div className="button previous">
+        <button>Предыдущий</button>
+    </div>
+);
+
+export default PreviousStep;
